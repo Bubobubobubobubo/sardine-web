f4102600dc8ca4b4e05b066ff47423d3d30cf08a
Raphael Forment <raphael.forment@gmail.com>
2023-07-24 12:11:00 +0200

    blob

--- sardine_web/server/flask_server.py
+++ sardine_web/server/flask_server.py
@@ -136,9 +136,72 @@ def server_factory(console):
     socketio = SocketIO(app)
     CORS(app, resources={r"/*": {"origins": "*"}})
 
-    @socketio.on('message')
-    def on_message(data):
-        exit()
+    @socketio.on("connect")
+    def on_connect():
+        sid = request.sid
+        print("New socket connected ", sid)
+
+    @socketio.on("join-room")
+    def on_join_room(data):
+        sid = request.sid
+        room_id = data["room_id"]
+        display_name = session[room_id]["name"]
+
+        # register sid to the room
+        join_room(room_id)
+        rooms_sid[sid] = room_id
+        names_sid[sid] = display_name
+
+        # broadcast to others in the room
+        print("[{}] New member joined: {}<{}>".format(room_id, display_name, sid))
+        emit("user-connect", {"sid": sid, "name": display_name},
+             broadcast=True, include_self=False, room=room_id)
+
+        # add to user list maintained on server
+        if room_id not in users_in_room:
+            users_in_room[room_id] = [sid]
+            emit("user-list", {"my_id": sid})  # send own id only
+        else:
+            usrlist = {u_id: names_sid[u_id]
+                       for u_id in users_in_room[room_id]}
+            # send list of existing users to the new member
+            emit("user-list", {"list": usrlist, "my_id": sid})
+            # add new member to user list maintained on server
+            users_in_room[room_id].append(sid)
+
+        print("\nusers: ", users_in_room, "\n")
+
+    @socketio.on("disconnect")
+    def on_disconnect():
+        sid = request.sid
+        room_id = rooms_sid[sid]
+        display_name = names_sid[sid]
+
+        print("[{}] Member left: {}<{}>".format(room_id, display_name, sid))
+        emit("user-disconnect", {"sid": sid},
+             broadcast=True, include_self=False, room=room_id)
+
+        users_in_room[room_id].remove(sid)
+        if len(users_in_room[room_id]) == 0:
+            users_in_room.pop(room_id)
+
+        rooms_sid.pop(sid)
+        names_sid.pop(sid)
+
+        print("\nusers: ", users_in_room, "\n")
+
+    @socketio.on("data")
+    def on_data(data):
+        sender_sid = data['sender_id']
+        target_sid = data['target_id']
+        if sender_sid != request.sid:
+            print("[Not supposed to happen!] request.sid and sender_id don't match!!!")
+
+        if data["type"] != "new-ice-candidate":
+            print('{} message from {} to {}'.format(
+                data["type"], sender_sid, target_sid))
+        socketio.emit('data', data, room=target_sid)
+
 
     @app.route("/save", methods=["POST"])
     def save_files_to_disk() -> str:
